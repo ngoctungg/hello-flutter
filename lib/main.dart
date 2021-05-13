78bc6864277147dd2c8f0c099ff0e987e2f6045b
Tugg <m.ngoctung@gmail.com>
2021-05-13 09:19:14 +0700

    day 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('HELLO!!!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('HELLO WORLD'),
+        ),
+        body: Column(
+          children: [
+            Text("What is L0 scramhing softw"),
+            RaisedButton(
+              onPressed: () => {},
+              child: Text("btn1"),
+            ),
+            RaisedButton(
+              onPressed: () => {},
+              child: Text("btn1"),
+            ),
+            ElevatedButton(
+              onPressed: () => {},
+              child: Text("btn1"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
